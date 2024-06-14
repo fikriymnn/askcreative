2705c6926bacdd2ddef2e912751cac59fbb26d12
Dafazan <dafazanlbs@gmail.com>
2024-06-14 21:12:12 +0700

    edit event card

--- src/components/home/services.jsx
+++ src/components/home/services.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import HoverEffect from '@/components/newcomps/service-card'
+import CardHoverEffectDemo from '@/components/newcomps/card-2d'
 
 function Services() {
   return (
@@ -8,97 +10,11 @@ function Services() {
       </p>
       <div
         id="getstart"
-        className="md:grid sm:grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4 md:pb-10 md:pt-6 md:px-10 pb-10 px-7 py-7"
+        className="w-full"
       >
-        <div className="group block md:rounded-lg sm:rounded-lg rounded-none md:px-7 md:py-4 p-0 md:bg-white sm:bg-white bg-blue-600 shadow-lg  md:hover:bg-blue-600 sm:hover:bg-blue-600 group-hover:text-white first-linexport service-list hover:bg-primary md:hover:text-white sm:hover:text-white cursor-pointer transition-all  border-2">
-          <div
-            className="flex items-baseline text-gray-900 md:text-[#007aff]  
-                      pl-[1px] pr-0 py-5"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-              className="service-icon icon-effect h-[2rem] lg:h-[2.5rem] w-auto"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-              ></path>
-            </svg>
-           
-            <a
-              href="/services/"
-              className="  md:border-none ml-[1px] text-[20px] font-semibold my-auto  md:text-black sm:text-black text-white  group-hover:text-white  pl-2 w-full "
-            >
-              Aromatherapy Workshop
-            </a>
-          </div>
-          <hr className="" />
-         
-        </div>
-   <div className="group block md:rounded-lg sm:rounded-lg rounded-none md:px-7 md:py-4 p-0 md:bg-white sm:bg-white bg-blue-600 shadow-lg  md:hover:bg-blue-600 sm:hover:bg-blue-600 group-hover:text-white first-linexport service-list hover:bg-primary md:hover:text-white sm:hover:text-white cursor-pointer transition-all  border-2">
-          <div
-            className="flex items-baseline text-gray-900 md:text-[#007aff]  
-                      pl-[1px] pr-0 py-5"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-              className="service-icon icon-effect h-[2rem] lg:h-[2.5rem] w-auto"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-              ></path>
-            </svg>
-            <a
-              href="/services/"
-              className="  md:border-none ml-[1px] text-[20px] font-semibold my-auto  md:text-black sm:text-black text-white  group-hover:text-white  pl-2 w-full "
-            >
-              Perfume Workshop
-            </a>
-          </div>
-          <hr className="" />
-        </div>
-      <div className="group block md:rounded-lg sm:rounded-lg rounded-none md:px-7 md:py-4 p-0 md:bg-white sm:bg-white bg-blue-600 shadow-lg  md:hover:bg-blue-600 sm:hover:bg-blue-600 group-hover:text-white first-linexport service-list hover:bg-primary md:hover:text-white sm:hover:text-white cursor-pointer transition-all  border-2">
-          <div
-            className="flex items-baseline text-gray-900 md:text-[#007aff]  
-                      pl-[1px] pr-0 py-5"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-              aria-hidden="true"
-              className="service-icon icon-effect h-[2rem] lg:h-[2.5rem] w-auto"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.42 15.17L17.25 21A2.652 2.652 0 0021 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 11-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 004.486-6.336l-3.276 3.277a3.004 3.004 0 01-2.25-2.25l3.276-3.276a4.5 4.5 0 00-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437l1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008z"
-              ></path>
-            </svg>
-            <a
-              href="/services/"
-              className="  md:border-none ml-[1px] text-[20px] font-semibold my-auto  md:text-black sm:text-black text-white  group-hover:text-white  pl-2 w-full "
-            >
-              Make your own Perfume and Aromatherapy
-            </a>
-          </div>
-          <hr className="" />
-        </div>
+        <CardHoverEffectDemo/>
+        
+        
       </div>
       
     </>
