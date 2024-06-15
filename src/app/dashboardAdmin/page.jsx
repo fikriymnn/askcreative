81dece60918727096c2821c60a0f50d1483e5076
Dafazan <dafazanlbs@gmail.com>
2024-06-15 13:36:17 +0700

    add overview events admin

--- src/app/dashboardAdmin/page.jsx
+++ src/app/dashboardAdmin/page.jsx
@@ -2,18 +2,43 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Navigation from "@/components/admin/navigation";
+import EventsDash from '@/components/newcomps/events-dash';
+import {
+  collection,
+  addDoc,
+  getDocs,
+  orderBy,
+  query,
+  where,
+  Firestore,
+} from "firebase/firestore";
+import { db, storage, firebaseAnalytics } from "../../../firebase/page";
+import EventPage from "@/components/events/eventPage";
 
-function page() {
+async function GetDataEvents() {
+  let data = [];
+  try {
+    const ordersRef = collection(db, "events");
+    const q = query(ordersRef, orderBy("createdAt", "desc"));
+    const querySnapshot = await getDocs(q);
+
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+
+      data.push({ ...doc.data(), id: doc.id });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  return data;
+}
+
+async function page() {
+  const dataEvents = await GetDataEvents();
+  
   return (
     <>
-      <div className="flex">
-        <Navigation />
-        <div className="w-full h-screen flex justify-center items-center p-5">
-          <div className="w-full h-full text-white bg-[#0d3064] py-7">
-            <p>Event Overview</p>
-          </div>
-        </div>
-      </div>
+      <EventsDash dataEvents={JSON.parse(JSON.stringify(dataEvents))} key={1}/>
     </>
   );
 }
