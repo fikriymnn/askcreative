9e7012af319d555790a5c6b4cdfd7dd0cd5b79c6
Dafazan <dafazanlbs@gmail.com>
2024-06-17 13:19:39 +0700

    add view registered people

--- src/app/dashboardAdmin/events/page.jsx
+++ src/app/dashboardAdmin/events/page.jsx
@@ -16,23 +16,44 @@ import { db, storage, firebaseAnalytics } from "../../../../firebase/page";
 
 import "@/components/admin/editor.css";
 import EventsAdminPage from "@/components/admin/events/eventsPage";
+// async function GetDataEvents() {
+//   let data = [];
+//   try {
+//     const ordersRef = collection(db, "events");
+//     const q = query(ordersRef, orderBy("createdAt", "desc"));
+//     const querySnapshot = await getDocs(q);
+
+//     querySnapshot.forEach((doc) => {
+//       // doc.data() is never undefined for query doc snapshots
+
+//       data.push({ ...doc.data(), id: doc.id });
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+//   return data;
+// }
+
 async function GetDataEvents() {
-  let data = [];
-  try {
-    const ordersRef = collection(db, "events");
-    const q = query(ordersRef, orderBy("createdAt", "desc"));
-    const querySnapshot = await getDocs(q);
+    let data = [];
+    try {
+        const ordersRef = collection(db, "events");
+        const q = query(ordersRef, orderBy("createdAt", "desc"));
+        const querySnapshot = await getDocs(q);
 
-    querySnapshot.forEach((doc) => {
-      // doc.data() is never undefined for query doc snapshots
+        for (const doc of querySnapshot.docs) {
+            const event = doc.data();
+            const registrantsSnapshot = await getDocs(collection(doc.ref, "registran"));
+            const registrants = registrantsSnapshot.docs.map(doc => doc.data());
 
-      data.push({ ...doc.data(), id: doc.id });
-    });
-  } catch (error) {
-    console.log(error);
-  }
-  return data;
+            data.push({ ...event, id: doc.id, registrants });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+    return data;
 }
+
 async function EventsAdmin() {
   const dataEvents = await GetDataEvents();
   return <EventsAdminPage data={JSON.parse(JSON.stringify(dataEvents))} />;
