0a9c47110035aa69d2c68b14f6d7c4c0b60cbdad
Dafazan <dafazanlbs@gmail.com>
2024-06-20 23:13:56 +0700

    new color

--- src/app/dashboardAdmin/events/create/page.jsx
+++ src/app/dashboardAdmin/events/create/page.jsx
@@ -346,23 +346,22 @@ function CreateEvent() {
                   required
                   onChange={(event) => handleUpload(event.target.files[0])}
                 />
-                <p className="text-red-600 pt-2">Image Ratio: 16:9 </p>
+                <p className="text-red-600 pt-2">Image Ratio: 1:1 </p>
                 <p className="text-red-600 pt-2">
-                  minimum image resolution: 1920 x 1080 pixel{" "}
+                  minimum image resolution: 1080 x 1080 pixel{" "}
                 </p>
               </div>
             </div>
             <div className=" flex py-1 px-20 ">
               <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
-                <p>Title</p>
+                <p>Details</p>
               </div>
               <div className=" w-10/12 "></div>
             </div>
             <div className=" flex py-1 px-20 ">
               <div className=" w-2/12 text-end p-3 py-5">
                 <p>
-                  Title
-                  <span className="text-red-600"> English</span> :
+                  Title:
                 </p>
               </div>
               <div className=" w-10/12 p-3">
@@ -387,7 +386,7 @@ function CreateEvent() {
                   onChange={(e) => setQuota(e.target.value)}
                   type="number"
                   required
-                  placeholder="Insert Title"
+                  placeholder="Insert Quota"
                   color=" bg-transparent"
                   className=" rounded-lg w-full border-slate-300 "
                 />
@@ -395,16 +394,8 @@ function CreateEvent() {
             </div>
 
             <div className=" flex py-1 px-20 ">
-              <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
-                <p>Location</p>
-              </div>
-              <div className=" w-10/12 "></div>
-            </div>
-
-            {/* location */}
-            <div className=" flex py-1 px-20 ">
               <div className=" w-2/12 text-end p-3 py-5">
-                <p>Loc :</p>
+                <p>Location :</p>
               </div>
               <div className=" w-10/12 p-3">
                 <input
@@ -414,6 +405,39 @@ function CreateEvent() {
                   placeholder="Insert Location"
                   color=" bg-transparent"
                   className=" rounded-lg w-full border-slate-300 "
+                />
+              </div>
+            </div>
+            <div className=" flex py-1 px-20 ">
+              <div className=" w-2/12 text-end p-3 py-5">
+                <p>Fee :</p>
+              </div>
+              <div className=" w-10/12 p-3">
+                <input
+                  onChange={(e) => setFeeRupiah(e.target.value)}
+                  type="text"
+                  required
+                  placeholder="Insert fee"
+                  color=" bg-transparent"
+                  className=" rounded-lg w-full border-slate-300 "
+                />
+              </div>
+            </div>
+            <div className=" flex py-1 px-20 ">
+              <div className=" w-2/12 text-end p-3 py-5">
+                <p>
+                  Description:
+                </p>
+              </div>
+              <div className=" w-10/12 p-3 mb-5">
+                <ReactQuill
+                  modules={modules}
+                  format={formats}
+                  onChange={(e) => setDesIng(e)}
+                  name="contentIng"
+                  placeholder={`Input Description English For Description ${1}`}
+                  maxLength={1000}
+                  className="h-[150px] w-full   "
                 />
               </div>
             </div>
@@ -489,55 +513,6 @@ function CreateEvent() {
                 />
               </div>
             </div>
-
-            {/* Fee */}
-            <div className=" flex py-1 px-20 ">
-              <div className=" w-2/12 text-end px-3 text-2xl font-semibold pt-5">
-                <p>Fee</p>
-              </div>
-              <div className=" w-10/12 "></div>
-            </div>
-            <div className=" flex py-1 px-20 ">
-              <div className=" w-2/12 text-end p-3 py-5">
-                <p>Rupiah :</p>
-              </div>
-              <div className=" w-10/12 p-3">
-                <input
-                  onChange={(e) => setFeeRupiah(e.target.value)}
-                  type="text"
-                  required
-                  placeholder="Insert fee"
-                  color=" bg-transparent"
-                  className=" rounded-lg w-full border-slate-300 "
-                />
-              </div>
-            </div>
-
-            <div className=" flex py-1 ps-24 pt-32 ">
-              <div className=" w-10/12 px-3 text-2xl font-semibold pt-5">
-                <p>Main Description:</p>
-              </div>
-            </div>
-            <div className=" flex py-1 px-20 ">
-              <div className=" w-2/12 text-end p-3 py-5">
-                <p>
-                  Description
-                  <span className="text-red-600"> English</span> :
-                </p>
-              </div>
-              <div className=" w-10/12 p-3">
-                <ReactQuill
-                  modules={modules}
-                  format={formats}
-                  onChange={(e) => setDesIng(e)}
-                  name="contentIng"
-                  placeholder={`Input Description English For Description ${1}`}
-                  maxLength={1000}
-                  className="h-[200px] w-full   "
-                />
-              </div>
-            </div>
-
             <div className=" flex py-1 px-20 ">
               <div className=" w-10/12 px-3 text-2xl font-semibold pt-5">
                 <p>Content</p>
@@ -556,8 +531,7 @@ function CreateEvent() {
                   <div className=" flex py-1 px-20 ">
                     <div className=" w-2/12 text-end p-3 py-5">
                       <p>
-                        Topic
-                        <span className="text-red-600"> English</span> :
+                        Header:
                       </p>
                     </div>
                     <div className=" w-10/12 p-3">
@@ -569,7 +543,7 @@ function CreateEvent() {
                         id=""
                         cols="20"
                         rows="1"
-                        placeholder={`Input Topic English For Description ${i + 1
+                        placeholder={`Input Header ${i + 1
                           }`}
                         color=" bg-transparent"
                         className=" w-full resize-none rounded-lg border-slate-300 "
@@ -581,8 +555,7 @@ function CreateEvent() {
                   <div className=" flex py-1 px-20 ">
                     <div className=" w-2/12 text-end p-3 py-5">
                       <p>
-                        Description
-                        <span className="text-red-600"> English</span> :
+                        Content:
                       </p>
                     </div>
                     <div className=" w-10/12 p-3">
@@ -600,10 +573,10 @@ function CreateEvent() {
                           )
                         }
                         name="contentIng"
-                        placeholder={`Input Description English For Description ${i + 1
+                        placeholder={`Input Content ${i + 1
                           }`}
                         maxLength={2000}
-                        className="h-[200px] "
+                        className="h-[200px] mb-5 "
                       />
                     </div>
                   </div>
@@ -632,8 +605,8 @@ function CreateEvent() {
                         </div>
                       );
                     })}
-                    <div className="flex justify-center items-center gap-10 mb-20">
-                      <div className="w-32 bg-blue-950 text-center rounded-xl text-white ">
+                    <div className="flex justify-start items-center gap-10 mb-20">
+                      <div className="px-3 bg-blue-950 text-center rounded-sm text-white mt-2">
                         <button
                           type="button"
                           onClick={() => {
@@ -666,7 +639,7 @@ function CreateEvent() {
                   onClick={handleClick}
                   className="font-light"
                 >
-                  Add More
+                  Add Content Section
                 </button>
               </div>
             </div>
