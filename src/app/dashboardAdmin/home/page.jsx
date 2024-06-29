9badf73497322a7667d15366ed5d52037911f530
Dafazan <dafazanlbs@gmail.com>
2024-06-29 17:45:25 +0700

    carousel landing

--- src/app/dashboardAdmin/home/page.jsx
+++ src/app/dashboardAdmin/home/page.jsx
@@ -25,12 +25,14 @@ function HomeAdmin() {
   const [dataLogoWhite, setDataLogoWhite] = useState([]);
   const [dataLogoNav, setDataLogoNav] = useState([]);
   const [dataLogoFoot, setDataLogoFoot] = useState([]);
+  const [dataHeading, setDataHeading] = useState([]);
   useEffect(() => {
     getDataHomeHeading();
     getDataHomeParagraph();
     getDataHomeLogoWhite();
     getDataHomeLogoNav();
     getDataHomeLogoFoot();
+    getDataHeading();
   }, []);
 
   //get data home page
@@ -52,6 +54,26 @@ function HomeAdmin() {
       data.push(querySnapshot.data());
 
       setDataHomeHeading(data);
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const getDataHeading = async () => {
+    try {
+      try {
+        const querySnapshot = await getDocs(collection(db, "heading"));
+        let data = [];
+        console.log(querySnapshot);
+        querySnapshot.forEach((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+          data.push({ ...doc.data(), id: doc.id });
+        });
+        setDataHeading(data);
+      } catch (error) {
+        alert(error);
+      }
     } catch (error) {
       alert(error);
     }
@@ -174,141 +196,111 @@ function HomeAdmin() {
                     <p>Edit</p>
                   </div>
                 </div>
-                <div className="flex bg-slate-300 rounded-md">
+
+                {dataHeading.map((data,i) => {
+                            return(
+                              <>
+                               <div className="flex w-full">
+                                <div className="w-full">
+
+                <div className="flex bg-slate-300 rounded-t-md w-full">
                   <div className="w-full flex">
                     <div className="w-2/12 font-semibold flex justify-start items-center p-2">
-                      <p>Heading</p>
+                      <p>Heading {i + 1}</p>
                     </div>
                     <div className="w-8/12 border-s-2  flex justify-start items-center p-2">
                       <div className="flex flex-col">
-                        {dataHomeHeading.length > 0 &&
+                        {/* {dataHomeHeading.length > 0 &&
                           dataHomeHeading.map((data, i) => {
                             return (
                               <>
                                 <p>{data.english}</p>
-                                <p>{data.chinese}</p>
+                              
                               </>
                             );
-                          })}
+                          })} */}
+                          <p>{data.heading}</p>
+                          
                       </div>
                     </div>
                   </div>
-                  <div className="w-14  flex gap-3 m-3 my-auto">
-                    <a
-                      className="bg-yellow-400 w-full h-10 rounded-md flex items-center justify-center m-auto"
-                      href="/dashboardAdmin/home/editHeading"
-                    >
-                      <button>
-                        <Image
-                          width={35}
-                          height={35}
-                          className="w-8"
-                          src="/assets/images/edit-svgrepo-com.svg"
-                          alt=""
-                        />
-                      </button>
-                    </a>
-                  </div>
+                  
                 </div>
 
-                <div className="flex bg-slate-300 rounded-md">
+                <div className="flex bg-slate-300 w-full ">
                   <div className="w-full flex">
                     <div className="w-2/12 font-semibold flex justify-start items-center p-2">
-                      <p>Paragraph</p>
+                      <p>Paragraph {i + 1}</p>
                     </div>
                     <div className="w-8/12 border-s-2  flex justify-start items-center p-2">
                       <div className="flex flex-col">
-                        {dataHomeParagraph.length > 0 &&
+                        {/* {dataHomeParagraph.length > 0 &&
                           dataHomeParagraph.map((data, i) => {
                             return (
                               <>
                                 <p>{data.english}</p>
-                                <p>{data.chinese}</p>
+                               
                               </>
                             );
-                          })}
+                          })} */}
+                          <p>{data.paragraph}</p>
                       </div>
                     </div>
                   </div>
+                 
+                </div>
+                <div className="flex bg-slate-300 rounded-b-md w-full">
+                  <div className="w-full flex">
+                    <div className="w-2/12 font-semibold flex justify-start items-center p-2">
+                      <p>Image {i + 1}</p>
+                    </div>
+                    <div className="w-8/12 border-s-2  flex justify-start items-center p-2">
+                      <div className="flex flex-col">
+                      <Image width={500} height={500} className="h-32 w-full" src={data.img} alt="" />
+                      </div>
+                    </div>
+                  </div>
+                 
+                </div>
+                                </div>
+                <div>
                   <div className="w-14  flex gap-3 m-3 my-auto">
-                    <a
-                      className="bg-yellow-400 w-full h-10 rounded-md flex items-center justify-center m-auto"
-                      href="/dashboardAdmin/home/editParagraph"
-                    >
-                      <button>
-                        <Image
-                          width={35}
-                          height={35}
-                          className="w-8"
-                          src="/assets/images/edit-svgrepo-com.svg"
-                          alt=""
-                        />
-                      </button>
-                    </a>
+                     <button
+                            onClick={async (e) => {
+                              const confirmed = window.confirm(
+                                "Are you sure you want to delete this item?"
+                              );
+                              if (confirmed) {
+                                try {
+                                  // Delete the todo document with the given ID from the "todos" collection in Firestore.
+                                  await deleteDoc(doc(db, "heading", data.id));
+                                  alert("delete success");
+                                  location.reload();
+                                } catch (error) {
+                                  console.error("An error occured", error);
+                                }
+                              }
+                            }}
+                            className="bg-red-600 h-10 rounded-md w-full p-3 flex items-center justify-center"
+                          >
+                            <Image
+                              src="/assets/images/delete-1-svgrepo-com.svg"
+                              width={25}
+                              height={25}
+                              alt=""
+                            />
+                          </button>
                   </div>
                 </div>
-
-                {/* <div className="flex bg-slate-300 rounded-md">
-                  <div className="w-full flex h-20">
-                    <div className="w-2/12 font-semibold flex justify-start items-center p-2">
-                      <p>Logo navbar white</p>
-                    </div>
-                    <div className="w-8/12 border-s-2  flex justify-start items-center p-2">
-                      <div className="flex flex-col">
-                        
-                      </div>
-                    </div>
-                  </div>
-                  <div className="w-14  flex gap-3 m-3 my-auto">
-                    <a
-                      className="bg-yellow-400 w-full h-10 rounded-md flex items-center justify-center m-auto"
-                      href="/dashboardAdmin/home/editLogoWhite"
-                    >
-                      <button>
-                        <Image
-                          width={35}
-                          height={35}
-                          className="w-8"
-                          src="/assets/images/edit-svgrepo-com.svg"
-                          alt=""
-                        />
-                      </button>
-                    </a>
-                  </div>
-                </div> */}
-
-                {/* <div className="flex bg-slate-300 rounded-md">
-                  <div className="w-full flex h-20">
-                    <div className="w-2/12 font-semibold flex justify-start items-center p-2">
-                      <p>Logo navbar</p>
-                    </div>
-                    <div className="w-8/12 border-s-2  flex justify-start items-center p-2">
-                      <div className="flex flex-col">
-                        {dataLogoNav.length > 0 &&
-                          dataLogoNav.map((data, i) => {
-                            return <>
-                              <Image width={220} height={100} src={data.img} alt="" /></>;
-                          })}
-                      </div>
-                    </div>
-                  </div>
-                  <div className="w-14  flex gap-3 m-3 my-auto">
-                    <a
-                      className="bg-yellow-400 w-full h-10 rounded-md flex items-center justify-center m-auto"
-                      href="/dashboardAdmin/home/editLogoNavbar"
-                    >
-                      <button>
-                        <Image
-                          width={35}
-                          height={35}
-                          className="w-8"
-                          src="/assets/images/edit-svgrepo-com.svg"
-                          alt=""
-                        />
-                      </button>
-                    </a>
-                  </div>
-                </div> */}
+                </div>
+                              </>
+                            )
+                })}
+              
+                <a href="/dashboardAdmin/home/addHeading" className="w-full">
+                  <button className="bg-green-500 p-5 text-center w-full text-white">ADD NEW</button>
+                </a>
+             
 
                 <div className="flex bg-slate-300 rounded-md">
                   <div className="w-full h-20 flex">
