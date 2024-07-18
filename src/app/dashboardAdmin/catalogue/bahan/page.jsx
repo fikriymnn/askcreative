2cae9673a8024fc7b7124399d6eadbacd9599b5b
Dafazan <dafazanlbs@gmail.com>
2024-07-18 20:49:39 +0700

    new input

---
+++ src/app/dashboardAdmin/catalogue/bahan/page.jsx
@@ -0,0 +1,10 @@
+
+import { db, storage, firebaseAnalytics } from "../../../../../firebase/page";
+import BahanPage from "@/components/catalogue/bahanPage";
+
+async function Bahan() {
+  
+  return <BahanPage />;
+}
+
+export default Bahan;
