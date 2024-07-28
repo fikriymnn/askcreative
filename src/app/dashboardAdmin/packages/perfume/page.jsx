d3b7cf2e8a42adce006f817133197932d0f41070
Dafazan <dafazanlbs@gmail.com>
2024-07-29 00:07:28 +0700

    new

--- src/app/dashboardAdmin/packages/perfume/page.jsx
+++ src/app/dashboardAdmin/packages/perfume/page.jsx
@@ -18,8 +18,46 @@ import {
 import { db, storage, firebaseAnalytics } from "../../../../../firebase/page";
 import Image from "next/image";
 
+import dynamic from "next/dynamic";
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "react-quill/dist/quill.snow.css";
+
 
 function Perfume() {
+
+const modules = {
+    toolbar: {
+      container: [
+        ["bold", "italic", "underline", "strike"], // toggled buttons
+        ["code-block", "link"],
+        [{ list: "ordered" }, { list: "bullet" }],
+        [{ script: "sub" }, { script: "super" }], // superscript/subscript
+        [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+        [{ direction: "rtl" }], // text direction
+        [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+        [{ align: [] }],
+        ["clean"],
+      ],
+    },
+  };
+  const formats = [
+    "strike",
+    "bold",
+    "italic",
+    "underline",
+    "link",
+    "align",
+    "direction",
+    "list",
+    "code-block",
+    "script",
+    "indent",
+    "direction",
+    "color",
+
+    "background",
+  ];
+  
 
   const [title, setTitle] = useState("");
   const [desIng, setDesIng] = useState("");
@@ -196,7 +234,17 @@ const handleButtonClick = (buttonId) => {
             </div>
              <div className="flex flex-col">
                 <p>Desctiption</p>
-                <input value={desIng} onChange={(e) => setDesIng(e.target.value)} type="text" name="img" className="w-full resize-none rounded-lg border-slate-300 "/>
+                 <ReactQuill
+                  modules={modules}
+                  value={desIng}
+                  format={formats}
+                  onChange={(e) => setDesIng(e)}
+                  name="contentIng"
+                  placeholder={`Input Description English For Description ${1}`}
+                  maxLength={1000}
+                  className="h-[200px] w-full   pb-10"
+                />
+                
             </div>
              <div className="flex flex-col">
                 <p>Events</p>
